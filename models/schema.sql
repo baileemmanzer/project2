b2bc89154306f7b727f831a11c5b2a70aa80cbca
Bailee Michaela <baileemanzer3@gmail.com>
2020-01-25 15:32:27 -0700

    added password. js file

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS fridge_friend_db;
+CREATE DATABASE fridge_friend_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE fridge_friend_db;
+
+CREATE TABLE shoppingList (
+    id int NOT NULL AUTO_INCREMENT,
+    product_name varchar(45) NOT NULL,
+    quantity integer NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE kitchenInventory(
+    id int NOT NULL AUTO_INCREMENT,
+    product_name varchar(45) NOT NULL,
+    quantity integer NULL, 
+    frequency integer NULL, 
+    expiration_date DATE NULL
+    PRIMARY KEY (id)
+);
