e5a0aa2af17d2ca6bcf3eaa87f3f3217669dc848
baileemmanzer <56013318+baileemmanzer@users.noreply.github.com>
2020-01-25 16:01:56 -0700

    Update schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,8 +1,12 @@
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
 DROP DATABASE IF EXISTS fridge_friend_db;
 CREATE DATABASE fridge_friend_db;
 
 USE fridge_friend_db;
 
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
 CREATE TABLE shoppingList (
     id int NOT NULL AUTO_INCREMENT,
     product_name varchar(45) NOT NULL,
