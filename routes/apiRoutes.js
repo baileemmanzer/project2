e1c3a0ad43433af59ea87b4d5a18c94590860304
baileemmanzer <56013318+baileemmanzer@users.noreply.github.com>
2020-01-25 15:57:25 -0700

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,24 +13,24 @@ module.exports = function(app) {
   );
   //Passport registry
 
-  // Get all examples - THIS IS BOILERPLATE CODE FOR IF WE END UP USING HANDLEBARS
-  // app.get("/api/examples", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
+  //Get all examples - THIS IS BOILERPLATE CODE FOR IF WE END UP USING HANDLEBARS
+   app.get("/api/examples", function(req, res) {
+     db.Example.findAll({}).then(function(dbExamples) {
+       res.json(dbExamples);
+     });
+   });
 
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+   // Create a new example
+   app.post("/api/examples", function(req, res) {
+     db.Example.create(req.body).then(function(dbExample) {
+       res.json(dbExample);
+     });
+   });
 
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+   // Delete an example by id
+   app.delete("/api/examples/:id", function(req, res) {
+     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+       res.json(dbExample);
+     });
+   });
 };
