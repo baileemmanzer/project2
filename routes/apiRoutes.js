3c6b621d90ad850b54786c031b70523ecd2ba6d5
Bailee Michaela <baileemanzer3@gmail.com>
2020-01-26 17:29:01 -0700

    password.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,46 @@
 var db = require("../models");
 var passport = require("..config/passport.js");
 
-module.exports = function(app) {
-  //Passport login
-  app.post(
-    "public/index.html",
-    passport.authenticate("local", {
-      successRedirect: "/",
-      failureRedirect: "public/index.html",
-      failureFlash: true
-    })
+//Passport routes
+app.get("/", function(req, res) {
+  if(req.user) {
+      res.send(
+          "<p>You\'re logged in as <strong>" + req.user.username + "</strong>.</p>"
+          + '<p><a href="/logout">Log out</a></p>'
+      );
+  }
+  else {
+      res.send('<p><a href="public/index.html">Login</a></p>');
+  }
+});
+
+app.get("public/index.html", function(req, res) {
+  res.send(
+      '<form action="public/index.html" method="POST">'
+      + '<h2>Login</h2>'
+      + '<p><input name="username"></p>'
+      + '<p><input name="password"></p>'
+      + '<p><input type="submit" value="Login"></p>'
+      + '<p style="color: red;">' + req.flash('error') + '</p>'
+      + '</form>'
+      
   );
+});
+
+app.get("public/index.html", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+//Passport login
+app.post(
+  "public/index.html",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "public/index.html",
+    failureFlash: true
+  })
+);
   //Passport registry
 
   // Get all examples - THIS IS BOILERPLATE CODE FOR IF WE END UP USING HANDLEBARS
