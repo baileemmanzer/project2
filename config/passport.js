9c0a79b801151cafc499e69a7eb81d59fe0b7bc8
Bailee Michaela <baileemanzer3@gmail.com>
2020-01-27 21:16:51 -0700

    added passport.js file

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+//Passport.js
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+var db = require("../models");
+var bcrypt = require("bcrypt-nodejs");
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+//Required for storing user info into session
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+//Required for retrieving user from session
+passport.deserializeUser(function(id, done) {
+  //User should be queried against db
+  //Using the ID
+  done(null, user);
+});
+
+module.exports = passport;
