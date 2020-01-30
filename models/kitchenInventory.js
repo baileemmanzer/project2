0884e0d83bcfa7f90ede03f3ea4826cff9a11597
Bailee Michaela <baileemanzer3@gmail.com>
2020-01-29 19:30:21 -0700

    created tables

---
+++ models/kitchenInventory.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+  var KitchenInventory = sequelize.define("Kitchen Inventory", {
+    ingredient: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      },
+      expirationDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+        validate: {
+          len: [1]
+        }
+      },
+      frequency: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+          len: [1]
+        }
+      }
+    }
+  });
+  KitchenInventory.associate = function(models) {
+    // We're saying that Kitchen Inventory belongs to a user
+    // Kitchen Inventory can't exist without a user due to the foreign key constraint
+    KitchenInventory.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  //syncs with DB
+  KitchenInventory.sync();
+  //creates the table
+  return KitchenInventory;
+};
---
+++ models/shoppingList.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+  var ShoppingList = sequelize.define("Shopping List", {
+    ingredient: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  ShoppingList.associate = function(models) {
+    ShoppingList.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  //syncs with DB
+  ShoppingList.sync();
+  //creates the table
+  return ShoppingList;
+};
