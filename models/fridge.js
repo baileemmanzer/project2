943cc90b5e6e441d65969a765eb460cdbfc02ef1
Bailee Michaela <baileemanzer3@gmail.com>
2020-01-29 22:16:42 -0700

    added association to fridge.js and deleted kitcheninventory.js because they are the same file

--- models/fridge.js
+++ models/fridge.js
@@ -35,5 +35,17 @@ module.exports = function(sequelize, DataTypes) {
       freezeTableName: true
     }
   );
+  KitchenInventory.associate = function(models) {
+    // We're saying that Kitchen Inventory belongs to a user
+    // Kitchen Inventory can't exist without a user due to the foreign key constraint
+    KitchenInventory.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  //syncs with DB
+  KitchenInventory.sync();
+  //creates the table
   return KitchenInventory;
 };
